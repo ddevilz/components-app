3f7257cdcb34d8d2dfcc32dd6428e754cd536af0
Devashish jadhav <jadhavom24@gmail.com>
2023-08-09 21:06:30 +0530

    tailwind css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className="flex justify-around h-24 items-center">
+    <header className=" flex justify-around h-24 items-center cursor-pointer">
       <div>
-        <h1 className="uppercase tracking-[1rem] font-black text-[25px] text-shadow-[4px_4px_10px_#6366f1]">Logo</h1>
+        <h1 className="mx-[30px] uppercase tracking-[1rem] font-black text-[25px] text-shadow-[4px_4px_10px_#6366f1]">
+          Logo
+        </h1>
       </div>
-      <div>
-        <nav>
-          <ul className="flex gap-10">
-            <li>Home</li>
-            <li>About</li>
-            <li>Shop
-              <ul className="hidden">
-                <li>Online</li>
-                <li>Time</li>
-              </ul>
-            </li>
-            <li>Contact Us</li>
+
+      <div className="relative">
+        <nav >
+          <FiMenu className="md:hidden mx-12" onClick={() => setOpen(!open)} />
+          <ul
+            className={`${
+              open ? "block w-full absolute bottom-[-10rem] md:relative md:bottom-[-5px]" : "hidden"
+            }  text-center md:flex gap-10`}
+          >
+            <li className="my-2 block">Home</li>
+            <li className="my-2 block">About</li>
+            <li className="my-2 block">Shop</li>
+            <li className="my-2 block">Contact Us</li>
           </ul>
         </nav>
       </div>
